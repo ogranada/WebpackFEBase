105957eee6e5fa3b649acb8943615f2505e3b8bf
Oscar Andrés Granada <oscar.andres.granada@gmail.com>
2018-08-19 20:14:29 -0500

    Data Cleaned

--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
 
 import { normalize } from 'normalize.css';
 import { data } from './main.scss';
-
-
-document.querySelector('.DesignArea-widgetsMenuTrigger').addEventListener('click', (evt) => {
-  evt.stopPropagation();
-  evt.preventDefault();
-  document.querySelector('.WidgetsMenu').classList.toggle('isHidden');
-  evt.target.innerText = evt.target.innerText === '<' ? '>' : '<' ;
-});
-document.querySelector('.DesignArea-widgetsMenuTrigger').innerText = '<';
