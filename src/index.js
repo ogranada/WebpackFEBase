7568faef60fc0abd5336a5b3c4f27677713223ee
Oscar Andrés Granada <oscar.andres.granada@gmail.com>
2018-09-16 22:08:25 -0500

    Updates

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 
 import { normalize } from 'normalize.css';
 import { data } from './main.scss';
+
+document.querySelector('.Header-menuTrigger').addEventListener('click', evt => {
+  evt.stopPropagation();
+  evt.preventDefault();
+  document.querySelector('.Header-navigation').classList.toggle('isOpen');
+});
+
+document.querySelector('.Header-menuCloseTrigger').addEventListener('click', () => 
+  document.querySelector('.Header-navigation').classList.remove('isOpen') );
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
 
 module.exports = {
   mode: process.env['MODE'] || 'development',
@@ -16,7 +18,16 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: 'My App',
       template: 'src/index.hbs'
-    })
+    }),
+    //*
+    new CopyWebpackPlugin([
+      {
+        from: 'src/assets',
+        to: 'assets',
+      },
+    ]),
+    //*/
+    new ImageminPlugin({ test: /\.(jpe?g|png|gif|svg)$/i })
   ],
   module: {
     rules: [
@@ -44,11 +55,23 @@ module.exports = {
           }
         }]
       },
+      /*
       {
         test: /\.(png|svg|jpg|gif)$/,
         use: [
           'file-loader'
         ]
+      },
+      //*/
+      {
+        test: /\.(png|jp(e*)g|svg|gif)$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 8000, // Convert images < 8kb to base64 strings
+            name: 'assets/[name].[ext].[ext]'
+          }
+        }]
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
